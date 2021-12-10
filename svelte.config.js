b51f96a509e73a4d26118df3ad2dc824048b4198
Damian Soto <soto.platero@gmail.com>
2021-12-10 04:59:16 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(),
+		adapter: vercel(),
 	}
 };
 
